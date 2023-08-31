d1d94177c0d06c32d795d922ecf4f82a75e8a211
Jari Honkanen <jaarhonka@yahoo.com>
2023-08-31 06:49:41 +0300

    test saving data to ram

--- testbenches/processor_w_ram_v2_tb.vhd
+++ testbenches/processor_w_ram_v2_tb.vhd
@@ -10,6 +10,7 @@ context vunit_lib.vunit_context;
     use work.test_programs_pkg.all;
     use work.ram_read_pkg.all;
     use work.ram_write_pkg.all;
+    use work.real_to_fixed_pkg.all;
 
 entity processor_w_ram_v2_tb is
   generic (runner_cfg : string);
@@ -18,7 +19,7 @@ end;
 architecture vunit_simulation of processor_w_ram_v2_tb is
 
     constant clock_period      : time    := 1 ns;
-    constant simtime_in_clocks : integer := 500;
+    constant simtime_in_clocks : integer := 1500;
     
     signal simulator_clock     : std_logic := '0';
     signal simulation_counter  : natural   := 0;
@@ -32,7 +33,7 @@ architecture vunit_simulation of processor_w_ram_v2_tb is
     constant test_program : program_array := get_dummy & get_low_pass_filter;
 
     signal program_counter : natural := test_program'high;
-    signal registers : realarray := (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 0.1, 0.0);
+    signal registers : realarray := (0.0, 0.10, 0.2, 0.3, 0.4, 0.5, 0.6, 0.1, 0.0);
 
     function init_ram(program : program_array) return ram_array
     is
@@ -47,8 +48,13 @@ architecture vunit_simulation of processor_w_ram_v2_tb is
 
     signal ram_contents   : ram_array             := init_ram(test_program);
     signal ram_read_port  : ram_read_port_record  := init_ram_read_port;
-    signal ram_write_port : ram_write_port_record := init_ram_write_port;
 
+    signal ram_read_data_port : ram_read_port_record  := init_ram_read_port;
+    signal ram_write_port     : ram_write_port_record := init_ram_write_port;
+
+    signal write_address    : natural := 40;
+    signal read_address     : natural := 40;
+    signal register_address : natural := 0;
 
 begin
 
@@ -71,6 +77,12 @@ begin
             program_counter <= dummy'length;
         end request_low_pass_filter;
 
+        procedure save_registers_to_ram is
+        begin
+            write_address <= 40-registers'length;
+            
+        end save_registers_to_ram;
+
     begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
@@ -79,6 +91,10 @@ begin
             if read_is_requested(ram_read_port) then
                 ram_read_port.data <= ram_contents(get_ram_address(ram_read_port));
             end if;
+            create_ram_read_port(ram_read_data_port);
+            if read_is_requested(ram_read_data_port) then
+                ram_read_data_port.data <= ram_contents(get_ram_address(ram_read_data_port));
+            end if;
             create_ram_write_port(ram_write_port);
             if write_is_requested(ram_write_port) then
                 ram_contents(get_write_address(ram_write_port)) <= ram_write_port.write_buffer;
@@ -86,12 +102,43 @@ begin
             ------------------------------
             request_data_from_ram(ram_read_port, program_counter);
             create_processor(program_counter , get_ram_data(ram_read_port) , registers);
-            if simulation_counter = 10 or decode(get_ram_data(ram_read_port)) = ready then
-                request_low_pass_filter;
+
+            if write_address < 40 then
+                write_address <= write_address + 1;
+                write_data_to_ram(ram_write_port, write_address, to_fixed(registers(write_address-(40-registers'length)), 19));
+            end if;
+
+            if read_address > 30 then
+                if read_address < 40 then
+                    read_address <= read_address + 1;
+                    request_data_from_ram(ram_read_data_port, read_address);
+                end if;
+            end if;
+
+            if ram_read_is_ready(ram_read_data_port) then
+                registers(register_address) <= to_real(get_ram_data(ram_read_data_port), 19);
+                register_address <= register_address + 1;
             end if;
 
             if decode(get_ram_data(ram_read_port)) = ready then
                 result <= registers(0);
+            end if;
+        ------------------------------------------------------------------------
+            if simulation_counter = 10 then
+                request_low_pass_filter;
+            end if;
+
+            if decode(get_ram_data(ram_read_port)) = ready then
+                save_registers_to_ram;
+            end if;
+
+            if write_address = 32 then
+                read_address <= 40-registers'length;
+                register_address <= 0;
+            end if;
+
+            if read_address = 39 then
+                request_low_pass_filter;
             end if;
 
         end if; -- rising_edge
--- vunit_run_sw_processor.py
+++ vunit_run_sw_processor.py
@@ -8,6 +8,8 @@ ROOT = Path(__file__).resolve().parent
 VU = VUnit.from_argv()
 
 testi = VU.add_library("testi")
+testi.add_source_files(ROOT / "source/hVHDL_fixed_point/real_to_fixed/real_to_fixed_pkg.vhd")
+testi.add_source_files(ROOT / "source/hVHDL_fixed_point/multiplier/multiplier_base_types_20bit_pkg.vhd")
 testi.add_source_files(ROOT / "source/hVHDL_fixed_point/multiplier/configuration/multiply_with_1_input_and_output_registers_pkg.vhd")
 testi.add_source_files(ROOT / "source/hVHDL_fixed_point/multiplier/multiplier_base_types_20bit_pkg.vhd")
 testi.add_source_files(ROOT / "source/hVHDL_fixed_point/multiplier/multiplier_pkg.vhd")
