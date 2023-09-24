22c4784624511403e0ad11c52640952cf68708ee
Jari Honkanen <jaarhonka@yahoo.com>
2023-09-24 16:22:22 +0300

    add interface to ram

--- ram/dual_port_ram.vhd
+++ ram/dual_port_ram.vhd
@@ -25,6 +25,32 @@ package ram_port_pkg is
         data            : std_logic_vector(ramtype'range);
         write_requested : std_logic;
     end record;
+
+    procedure init_ram (
+        signal self_read_in : out ram_read_in_record);
+
+    procedure init_ram (
+        signal self_write_in : out ram_write_in_record);
+
+    procedure init_ram (
+        signal self_read_in : out ram_read_in_record;
+        signal self_write_in : out ram_write_in_record);
+
+    procedure request_data_from_ram (
+        signal self_read_in : out ram_read_in_record;
+        address : in natural);
+
+    function ram_read_is_ready ( self_read_out : ram_read_out_record)
+        return boolean;
+
+    function get_ram_data ( self_read_out : ram_read_out_record)
+        return std_logic_vector;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+    procedure write_data_to_ram (
+        signal self_write_in : out ram_write_in_record;
+        address : in natural;
+        data : in std_logic_vector);
         -- clka  : in std_logic;                                       -- Clock
         -- ena   : in std_logic;                                       -- Port A RAM Enable
         -- wea   : in std_logic;                                       -- Port A Write enable
@@ -41,11 +67,82 @@ package ram_port_pkg is
         -- regceb: in std_logic;                       			-- Port B Output register enable
         -- doutb : out std_logic_vector(RAM_WIDTH-1 downto 0)   		-- Port B RAM output data
 
-
-
 end package ram_port_pkg;
 
 package body ram_port_pkg is
+
+------------------------------------------------------------------------
+    procedure init_ram
+    (
+        signal self_read_in : out ram_read_in_record
+    ) is
+    begin
+        self_read_in.read_is_requested <= '0';
+    end init_ram;
+
+    procedure init_ram
+    (
+        signal self_write_in : out ram_write_in_record
+    ) is
+    begin
+        self_write_in.write_requested  <= '0';
+    end init_ram;
+
+    procedure init_ram
+    (
+        signal self_read_in : out ram_read_in_record;
+        signal self_write_in : out ram_write_in_record
+    ) is
+    begin
+        init_ram(self_read_in);
+        init_ram(self_write_in);
+    end init_ram;
+------------------------------
+------------------------------
+    procedure request_data_from_ram
+    (
+        signal self_read_in : out ram_read_in_record;
+        address : in natural
+    ) is
+    begin
+        self_read_in.address <= address;
+        self_read_in.read_is_requested <= '1';
+    end request_data_from_ram;
+------------------------------
+    function ram_read_is_ready
+    (
+        self_read_out : ram_read_out_record
+    )
+    return boolean
+    is
+    begin
+        return self_read_out.data_is_ready = '1';
+        
+    end ram_read_is_ready;
+------------------------------
+    function get_ram_data
+    (
+        self_read_out : ram_read_out_record
+    )
+    return std_logic_vector 
+    is
+    begin
+        return self_read_out.data;
+    end get_ram_data;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+    procedure write_data_to_ram
+    (
+        signal self_write_in : out ram_write_in_record;
+        address : in natural;
+        data : in std_logic_vector
+    ) is
+    begin
+        self_write_in.address <= address;
+        self_write_in.data <= data;
+        self_write_in.write_requested <= '1';
+    end write_data_to_ram;
+------------------------------------------------------------------------
 
 
 end package body ram_port_pkg;
