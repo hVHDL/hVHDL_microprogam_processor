692a59a39b12e4dc19ec7ef2bed8be7a3cb89db0
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-26 07:14:09 +0300

    use arguments in ghdl compile

--- ghdl_compile.bat
+++ ghdl_compile.bat
@@ -4,6 +4,6 @@ rem call source/hVHDL_memory_library/ghdl_compile_memory_library.bat source/hVHD
 call source/hVHDL_fixed_point/ghdl_compile_math_library.bat source/hVHDL_fixed_point
 call source/hVHDL_memory_library/ghdl_compile_memory_library.bat source/hVHDL_memory_library
 
-ghdl -a --ieee=synopsys --std=08 testbenches/microinstruction_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 testbenches/test_programs_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 testbenches/microcode_processor_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/testbenches/microinstruction_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/testbenches/test_programs_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/testbenches/microcode_processor_pkg.vhd
