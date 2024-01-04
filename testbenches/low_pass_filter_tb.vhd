ea6a7adb2219860a011bf402dbd4f946efa8a74a
Jari Honkanen <jaarhonka@yahoo.com>
2024-01-04 21:01:07 +0200

    move simple processor into own procedure

--- testbenches/low_pass_filter_tb.vhd
+++ testbenches/low_pass_filter_tb.vhd
@@ -28,6 +28,14 @@ package simple_processor_pkg is
     end record;
 
     function init_processor return simple_processor_record;
+
+    procedure create_simple_processor (
+        signal self                    : inout simple_processor_record;
+        signal ram_read_instruction_in : out ram_read_in_record    ;
+        ram_read_instruction_out       : in ram_read_out_record    ;
+        signal ram_read_data_in        : out ram_read_in_record    ;
+        ram_read_data_out              : in ram_read_out_record    ;
+        signal ram_write_port          : out ram_write_in_record);
     
     function "+" ( left, right : std_logic_vector )
     return std_logic_vector ;
@@ -107,6 +115,122 @@ package body simple_processor_pkg is
     begin
         return std_logic_vector(radix_multiply(signed(left), signed(right), 19));
     end "*";
+
+
+    procedure create_simple_processor
+    (
+        signal self                    : inout simple_processor_record;
+        signal ram_read_instruction_in : out ram_read_in_record    ;
+        ram_read_instruction_out       : in ram_read_out_record    ;
+        signal ram_read_data_in        : out ram_read_in_record    ;
+        ram_read_data_out              : in ram_read_out_record    ;
+        signal ram_write_port          : out ram_write_in_record
+    ) is
+        variable used_instruction : t_instruction;
+    begin
+            init_ram(ram_read_instruction_in, ram_read_data_in, ram_write_port);
+        ------------------------------------------------------------------------
+            --stage -1
+
+            used_instruction := write_instruction(nop);
+            if self.processor_enabled then
+                request_data_from_ram(ram_read_instruction_in, self.program_counter);
+
+                if ram_read_is_ready(ram_read_instruction_out) then
+                    used_instruction := get_ram_data(ram_read_instruction_out);
+                end if;
+
+                if decode(used_instruction) = program_end then
+                    self.processor_enabled <= false;
+                    used_instruction := write_instruction(nop);
+                else
+                    self.program_counter <= self.program_counter + 1;
+                end if;
+            end if;
+        ------------------------------------------------------------------------
+            CASE decode(used_instruction) is
+                WHEN load =>
+                    request_data_from_ram(ram_read_data_in, get_sigle_argument(used_instruction));
+
+                -- WHEN stall =>
+                --     self.stall_counter   <= get_long_argument(used_instruction);
+                --     self.program_counter <= self.program_counter - 3;
+                --     used_instruction := write_instruction(nop);
+                --
+                -- WHEN write_pc =>
+                --     self.registers(0) <= std_logic_vector(to_unsigned(self.program_counter-3,self.registers(0)'length));
+                WHEN others => -- do nothing
+            end CASE;
+            -- if self.stall_counter > 0 then
+            --     self.stall_counter   <= self.stall_counter - 1;
+            --     self.program_counter <= self.program_counter;
+            --     used_instruction := write_instruction(nop);
+            -- end if;
+
+            self.instruction_pipeline <= used_instruction & self.instruction_pipeline(0 to self.instruction_pipeline'high-1);
+        ------------------------------------------------------------------------
+        ------------------------------------------------------------------------
+            --stage 0
+            used_instruction := self.instruction_pipeline(0);
+
+            CASE decode(used_instruction) is
+                WHEN add =>
+                    self.add_a <= self.registers(get_arg1(used_instruction));
+                    self.add_b <= self.registers(get_arg2(used_instruction));
+                WHEN sub =>
+                    self.add_a <=  self.registers(get_arg1(used_instruction));
+                    self.add_b <= -self.registers(get_arg2(used_instruction));
+                WHEN mpy =>
+                    self.mpy_a <= self.registers(get_arg1(used_instruction));
+                    self.mpy_b <= self.registers(get_arg2(used_instruction));
+
+                WHEN jump =>
+                    self.program_counter <= get_long_argument(self.instruction_pipeline(0));
+
+                WHEN others => -- do nothing
+            end CASE;
+        ------------------------------------------------------------------------
+            --stage 1
+            used_instruction := self.instruction_pipeline(1);
+            self.add_result <= self.add_a + self.add_b;
+            self.mpy_a1     <= self.mpy_a;
+            self.mpy_b1     <= self.mpy_b;
+        ------------------------------------------------------------------------
+            --stage 2
+            used_instruction := self.instruction_pipeline(2);
+            self.mpy_raw_result <= signed(self.mpy_a1) * signed(self.mpy_b1);
+
+            CASE decode(used_instruction) is
+                WHEN load =>
+                    self.registers(get_dest(used_instruction)) <= get_ram_data(ram_read_data_out);
+                WHEN add | sub =>
+                    self.registers(get_dest(used_instruction)) <= self.add_result;
+
+                WHEN others => -- do nothing
+            end CASE;
+        ------------------------------------------------------------------------
+            --stage 3
+            used_instruction := self.instruction_pipeline(3);
+            self.mpy_result <= std_logic_vector(self.mpy_raw_result(38 downto 38-19));
+
+            CASE decode(used_instruction) is
+                WHEN save =>
+                    write_data_to_ram(ram_write_port, get_sigle_argument(used_instruction), self.registers(get_dest(used_instruction)));
+
+                WHEN others => -- do nothing
+            end CASE;
+        ------------------------------------------------------------------------
+            --stage 4
+            used_instruction := self.instruction_pipeline(4);
+            CASE decode(used_instruction) is
+                WHEN mpy =>
+                    self.registers(get_dest(used_instruction)) <= self.mpy_result;
+                WHEN others => -- do nothing
+            end CASE;
+            --stage 5
+            used_instruction := self.instruction_pipeline(5);
+        ------------------------------------------------------------------------
+    end create_simple_processor;
 
 end package body simple_processor_pkg;
 ------------------------------------------------------------------------
@@ -245,7 +369,6 @@ begin
     stimulus : process(simulator_clock)
     ------------------------------------------------------------------------
     ------------------------------------------------------------------------
-        variable used_instruction : t_instruction;
 
         procedure request_program is
         begin
@@ -258,120 +381,13 @@ begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
             --------------------
-            init_ram(ram_read_instruction_in, ram_read_data_in, ram_write_port);
-        --------------------------------------------------
-        ------------------------------------------------------------------------
-            --stage -1
-
-            if self.processor_enabled then
-                request_data_from_ram(ram_read_instruction_in, self.program_counter);
-
-                if ram_read_is_ready(ram_read_instruction_out) then
-                    used_instruction := get_ram_data(ram_read_instruction_out);
-                end if;
-
-                if decode(used_instruction) = program_end then
-                    self.processor_enabled <= false;
-                    used_instruction := write_instruction(nop);
-                else
-                    self.program_counter <= self.program_counter + 1;
-                end if;
-            else
-                used_instruction := write_instruction(nop);
-            end if;
-
-
-            CASE decode(used_instruction) is
-                WHEN load =>
-                    request_data_from_ram(ram_read_data_in, get_sigle_argument(used_instruction));
-
-                -- WHEN stall =>
-                --     self.stall_counter   <= get_long_argument(used_instruction);
-                --     self.program_counter <= self.program_counter - 3;
-                --     used_instruction := write_instruction(nop);
-                --
-                -- WHEN write_pc =>
-                --     self.registers(0) <= std_logic_vector(to_unsigned(self.program_counter-3,self.registers(0)'length));
-                WHEN others => -- do nothing
-            end CASE;
-
-
-            -- if self.stall_counter > 0 then
-            --     self.stall_counter   <= self.stall_counter - 1;
-            --     self.program_counter <= self.program_counter;
-            --     used_instruction := write_instruction(nop);
-            -- end if;
-
-            self.instruction_pipeline <= used_instruction & self.instruction_pipeline(0 to self.instruction_pipeline'high-1);
-        ------------------------------------------------------------------------
-        ------------------------------------------------------------------------
-            --stage 0
-            used_instruction := self.instruction_pipeline(0);
-
-            CASE decode(used_instruction) is
-                WHEN add =>
-                    self.add_a <= self.registers(get_arg1(used_instruction));
-                    self.add_b <= self.registers(get_arg2(used_instruction));
-                WHEN sub =>
-                    self.add_a <=  self.registers(get_arg1(used_instruction));
-                    self.add_b <= -self.registers(get_arg2(used_instruction));
-                WHEN mpy =>
-                    self.mpy_a <= self.registers(get_arg1(used_instruction));
-                    self.mpy_b <= self.registers(get_arg2(used_instruction));
-
-                WHEN jump =>
-                    self.program_counter <= get_long_argument(self.instruction_pipeline(0));
-
-                WHEN others => -- do nothing
-            end CASE;
-
-        ------------------------------------------------------------------------
-            --stage 1
-            used_instruction := self.instruction_pipeline(1);
-            self.add_result <= self.add_a + self.add_b;
-            self.mpy_a1     <= self.mpy_a;
-            self.mpy_b1     <= self.mpy_b;
-
-
-        ------------------------------------------------------------------------
-            --stage 2
-            used_instruction := self.instruction_pipeline(2);
-            self.mpy_raw_result <= signed(self.mpy_a1) * signed(self.mpy_b1);
-
-            CASE decode(used_instruction) is
-                WHEN load =>
-                    self.registers(get_dest(used_instruction)) <= get_ram_data(ram_read_data_out);
-                WHEN add | sub =>
-                    self.registers(get_dest(used_instruction)) <= self.add_result;
-
-                WHEN others => -- do nothing
-            end CASE;
-
-        ------------------------------------------------------------------------
-            --stage 3
-            used_instruction := self.instruction_pipeline(3);
-            self.mpy_result <= std_logic_vector(self.mpy_raw_result(38 downto 38-19));
-
-            CASE decode(used_instruction) is
-                WHEN save =>
-                    write_data_to_ram(ram_write_port, get_sigle_argument(used_instruction), self.registers(get_dest(used_instruction)));
-
-                WHEN others => -- do nothing
-            end CASE;
-        ------------------------------------------------------------------------
-            --stage 4
-            used_instruction := self.instruction_pipeline(4);
-            CASE decode(used_instruction) is
-                WHEN mpy =>
-                    self.registers(get_dest(used_instruction)) <= self.mpy_result;
-                WHEN others => -- do nothing
-            end CASE;
-        ------------------------------------------------------------------------
-            --stage 5
-            used_instruction := self.instruction_pipeline(5);
-        ------------------------------------------------------------------------
-
-        ------------------------------------------------------------------------
+            create_simple_processor (
+                self                    ,
+                ram_read_instruction_in ,
+                ram_read_instruction_out,
+                ram_read_data_in        ,
+                ram_read_data_out       ,
+                ram_write_port);
         ------------------------------------------------------------------------
             if simulation_counter mod 55 = 0 then
                 request_program;
