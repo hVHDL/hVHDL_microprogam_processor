a8f15eacbdf043c81068c010e7f5029e041bf1fe
Jari Honkanen <jaarhonka@yahoo.com>
2024-01-13 08:31:57 +0200

    separate float and fixed point bit width packages

---
+++ processor_configuration/float_processor_ram_width_pkg.vhd
@@ -0,0 +1,20 @@
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+    -- used in simple processor simulation
+
+package ram_configuration_pkg is
+
+    -- make visible when using ram_read
+    constant ram_bit_width : natural := 33;
+    constant ram_depth     : natural := 2**9;
+
+    subtype address_integer is natural range 0 to ram_depth-1;
+    subtype t_ram_data      is std_logic_vector(ram_bit_width-1 downto 0);
+
+    type ram_array is array (integer range 0 to ram_depth-1) of t_ram_data;
+
+end package ram_configuration_pkg;
+------------------------------------------------------------------------
