d9a86a2266847e45dc034bde8a51f7a6ca2f9743
Jari Honkanen <jaarhonka@yahoo.com>
2024-01-16 07:17:05 +0200

    add configuration package for float processor word length

---
+++ processor_configuration/processor_float_word_length_pkg.vhd
@@ -0,0 +1,12 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+
+-- float processor word length definitions
+package float_word_length_pkg is
+
+    constant mantissa_bits : integer := 24;
+    constant exponent_bits : integer := 8;
+
+end package float_word_length_pkg;
