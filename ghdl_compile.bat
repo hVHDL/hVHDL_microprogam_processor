0a10c7a11027c0056bb7c85889657d6301dd4870
Jari Honkanen <jaarhonka@yahoo.com>
2024-03-20 07:46:18 +0200

    fix ghdl_compile to use passed argument

--- ghdl_compile.bat
+++ ghdl_compile.bat
@@ -1,24 +1,24 @@
 echo off
 
 if "%1"=="" (
-    set src=./
+    set microprogram_src=./
 ) else (
-    set src=%1
+    set microprogram_src=%1
 )
 
-call source/hVHDL_fixed_point/ghdl_compile_math_library.bat source/hVHDL_fixed_point
-call source/hVHDL_floating_point/ghdl_compile_vhdl_float.bat source/hVHDL_floating_point/
+call %microprogram_src%/source/hVHDL_fixed_point/ghdl_compile_math_library.bat %microprogram_src%/source/hVHDL_fixed_point
+call %microprogram_src%/source/hVHDL_floating_point/ghdl_compile_vhdl_float.bat %microprogram_src%/source/hVHDL_floating_point/
 
-ghdl -a --ieee=synopsys --std=08 %src%/processor_configuration/ram_configuration_for_simple_processor_pkg.vhd
-call source/hVHDL_memory_library/ghdl_compile_memory_library.bat source/hVHDL_memory_library/
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/processor_configuration/ram_configuration_for_simple_processor_pkg.vhd
+call %microprogram_src%/source/hVHDL_memory_library/ghdl_compile_memory_library.bat %microprogram_src%/source/hVHDL_memory_library/
 
-ghdl -a --ieee=synopsys --std=08 %src%/processor_configuration/processor_configuration_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/vhdl_assembler/microinstruction_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/processor_configuration/fixed_point_command_pipeline_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/simple_processor/simple_processor_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/simple_processor/test_programs_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/vhdl_assembler/float_assembler_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/processor_configuration/float_pipeline_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/simple_processor/float_example_program_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/memory_processor/memory_processing_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %src%/memory_processor/memory_processor.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/processor_configuration/processor_configuration_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/vhdl_assembler/microinstruction_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/processor_configuration/fixed_point_command_pipeline_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/simple_processor/simple_processor_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/simple_processor/test_programs_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/vhdl_assembler/float_assembler_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/processor_configuration/float_pipeline_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/simple_processor/float_example_program_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/memory_processor/memory_processing_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %microprogram_src%/memory_processor/memory_processor.vhd
