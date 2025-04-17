41e4a9b419702b83bd5b1a872078eabab703d095
Jari Honkanen <jaarhonka@yahoo.com>
2025-04-17 06:04:28 +0300

    bump submodules and start adding generic packages for vhdl2008 style configuration

---
+++ vhdl2008/vhdl2008_config_pkg.vhd
@@ -0,0 +1,404 @@
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+package microinstruction_pkg is
+    generic( ram_bit_width             : natural := 32
+            ;instruction_bit_width     : natural := ram_bit_width
+            ;register_bit_width        : natural := ram_bit_width
+            ;number_of_registers       : natural := 5
+            ;number_of_pipeline_stages : natural := 10);
+
+    type t_command is (
+        program_end ,
+        nop         ,
+        add         ,
+        sub         ,
+        mpy         ,
+        mpy_add     ,
+        neg_mpy_add ,
+        mpy_sub     ,
+
+        save        ,
+        load
+    );
+
+    subtype comm is std_logic_vector(31 downto 28);
+    subtype dest is std_logic_vector(27 downto 21);
+    subtype arg1 is std_logic_vector(20 downto 14);
+    subtype arg2 is std_logic_vector(13 downto 7);
+    subtype arg3 is std_logic_vector(6 downto 0);
+    subtype long_arg is std_logic_vector(27 downto 0);
+
+    type reg_array                  is array (integer range 0 to number_of_registers-1) of std_logic_vector(register_bit_width-1 downto 0);
+    subtype t_instruction           is std_logic_vector(instruction_bit_width-1 downto 0);
+    type instruction_pipeline_array is array (integer range 0 to number_of_pipeline_stages-1) of t_instruction;
+    type program_array              is array (natural range <>) of t_instruction;
+
+------------------------------------------------------------------------
+    -- these are used to help with using internal variables in microprograms
+    type variable_array is array (natural range <>) of integer;
+
+    function init_variables ( number_of_variables : natural)
+        return variable_array;
+
+    function "+" ( left : variable_array; right : integer)
+        return variable_array;
+
+------------------------------------------------------------------------
+    function write_instruction ( command : in t_command)
+        return t_instruction;
+------------------------------------------------------------------------
+    function write_instruction (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural ;
+        argument2   : in natural )
+    return t_instruction;
+----------------
+    function write_instruction (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural ;
+        argument2   : in natural ;
+        argument3   : in natural )
+    return t_instruction;
+----------------
+    function write_instruction (
+        command     : in t_command;
+        long_argument : in natural)
+    return t_instruction;
+
+------------------------------------------------------------------------
+    function write_instruction (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural)
+    return t_instruction;
+
+------------------------------------------------------------------------
+    function get_sigle_argument (
+        input_register : std_logic_vector )
+    return t_instruction;
+
+------------------------------------------------------------------------
+    function get_sigle_argument (
+        input_register : std_logic_vector )
+    return natural;
+------------------------------------------------------------------------
+    function get_instruction ( input_register : std_logic_vector )
+        return integer;
+------------------------------------------------------------------------
+    function decode ( number : natural)
+        return t_command;
+------------------------------------------------------------------------
+    function decode ( number : std_logic_vector)
+        return t_command;
+------------------------------------------------------------------------
+    function get_dest ( input_register : std_logic_vector )
+        return natural;
+------------------------------------------------------------------------
+    function get_arg1 ( input_register : std_logic_vector )
+        return natural;
+------------------------------------------------------------------------
+    function get_arg2 ( input_register : std_logic_vector )
+        return natural;
+------------------------------------------------------------------------
+    function get_arg3 ( input_register : std_logic_vector )
+        return natural;
+------------------------------------------------------------------------
+    function get_long_argument ( input_register : std_logic_vector )
+        return natural;
+    function get_long_argument ( input_register : std_logic_vector )
+        return t_instruction;
+------------------------------------------------------------------------
+    function pipelined_block ( program : program_array)
+        return program_array;
+------------------------------------------------------------------------
+    function pipelined_block ( instruction : t_instruction)
+        return program_array;
+------------------------------------------------------------------------
+end package microinstruction_pkg;
+
+package body microinstruction_pkg is
+------------------------------------------------------------------------
+    constant ref : std_logic_vector(dest'low-1 downto 0) := (others => '0');
+
+    function write_instruction
+    (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural ;
+        argument2   : in natural ;
+        argument3   : in natural 
+    )
+    return t_instruction
+    is
+        variable instruction : t_instruction := (others=>'0');
+    begin
+
+        instruction(comm'range) := std_logic_vector(to_unsigned(t_command'pos(command) , comm'length));
+        instruction(dest'range) := std_logic_vector(to_unsigned(destination            , dest'length));
+        instruction(arg1'range) := std_logic_vector(to_unsigned(argument1              , arg1'length));
+        instruction(arg2'range) := std_logic_vector(to_unsigned(argument2              , arg2'length));
+        instruction(arg3'range) := std_logic_vector(to_unsigned(argument3              , arg3'length));
+
+        return instruction;
+        
+    end write_instruction;
+------------------------------------------------------------------------
+    function write_instruction
+    (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural ;
+        argument2   : in natural 
+    )
+    return t_instruction
+    is
+        variable instruction : t_instruction := (others=>'0');
+    begin
+
+        instruction(comm'range) := std_logic_vector(to_unsigned(t_command'pos(command) , comm'length));
+        instruction(dest'range) := std_logic_vector(to_unsigned(destination            , dest'length));
+        instruction(arg1'range) := std_logic_vector(to_unsigned(argument1              , arg1'length));
+        instruction(arg2'range) := std_logic_vector(to_unsigned(argument2              , arg2'length));
+
+        return instruction;
+        
+    end write_instruction;
+
+------------------------------------------------------------------------
+    function write_instruction
+    (
+        command     : in t_command;
+        destination : in natural ;
+        argument1   : in natural
+    )
+    return t_instruction
+    is
+        variable instruction : t_instruction := (others=>'0');
+    begin
+
+        instruction(comm'range)        := std_logic_vector(to_unsigned(t_command'pos(command) , comm'length));
+        instruction(dest'range)        := std_logic_vector(to_unsigned(destination            , dest'length));
+        instruction(dest'low-1 downto 0) := std_logic_vector(to_unsigned(argument1            , ref'length));
+
+        return instruction;
+        
+    end write_instruction;
+------------------------------------------------------------------------
+    function write_instruction
+    (
+        command     : in t_command;
+        long_argument : in natural
+    )
+    return t_instruction
+    is
+        variable instruction : t_instruction := (others=>'0');
+    begin
+
+        instruction(comm'range) := std_logic_vector(to_unsigned(t_command'pos(command) , comm'length));
+        instruction(long_arg'range) := std_logic_vector(to_unsigned(long_argument, long_arg'length));
+
+        return instruction;
+        
+    end write_instruction;
+------------------------------------------------------------------------
+    function write_instruction
+    (
+        command : in t_command
+    )
+    return t_instruction
+    is
+        variable instruction : t_instruction := (others=>'0');
+    begin
+
+        return write_instruction(command, 3,0,1);
+        
+    end write_instruction;
+------------------------------------------------------------------------
+    function get_dest
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+    begin
+        return to_integer(unsigned(input_register(dest'range)));
+    end get_dest;
+------------------------------------------------------------------------
+    function get_arg1
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+    begin
+        return to_integer(unsigned(input_register(arg1'range)));
+    end get_arg1;
+------------------------------------------------------------------------
+    function get_arg2
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+    begin
+        return to_integer(unsigned(input_register(arg2'range)));
+    end get_arg2;
+------------------------------------------------------------------------
+    function get_arg3
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+    begin
+        return to_integer(unsigned(input_register(arg3'range)));
+    end get_arg3;
+------------------------------------------------------------------------
+    function get_long_argument
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+    begin
+        return to_integer(unsigned(input_register(comm'low-1 downto 0)));
+        
+    end get_long_argument;
+
+------------------------------------------------------------------------
+    function get_long_argument
+    (
+        input_register : std_logic_vector 
+    )
+    return t_instruction
+    is
+        variable retval : t_instruction := (others => '0');
+    begin
+        retval(comm'low-1 downto 0) := input_register(comm'low-1 downto 0);
+        return retval;
+        
+    end get_long_argument;
+
+------------------------------------------------------------------------
+    function get_sigle_argument
+    (
+        input_register : std_logic_vector 
+    )
+    return t_instruction
+    is
+        variable retval : t_instruction := (others => '0');
+    begin
+        retval(ref'range) := input_register(ref'range);
+        return retval;
+        
+    end get_sigle_argument;
+
+------------------------------------------------------------------------
+    function get_sigle_argument
+    (
+        input_register : std_logic_vector 
+    )
+    return natural
+    is
+        variable retval : t_instruction := (others => '0');
+    begin
+        retval(ref'range) := input_register(ref'range);
+        return to_integer(unsigned(retval));
+        
+    end get_sigle_argument;
+------------------------------------------------------------------------
+    function get_instruction
+    (
+        input_register : std_logic_vector 
+    )
+    return integer
+    is
+    begin
+        return to_integer(unsigned(input_register(comm'range)));
+    end get_instruction;
+------------------------------------------------------------------------
+    function decode
+    (
+        number : natural
+    )
+    return t_command
+    is
+    begin
+        return t_command'val(number);
+    end decode;
+------------------------------------------------------------------------
+    function decode
+    (
+        number : std_logic_vector
+    )
+    return t_command
+    is
+    begin
+        return decode(get_instruction(number));
+    end decode;
+------------------------------------------------------------------------
+    function pipelined_block
+    (
+        program : program_array
+    )
+    return program_array
+    is
+        variable retval : program_array(0 to number_of_pipeline_stages-1) := (others => write_instruction(nop));
+    begin
+
+        if program'length < retval'length then
+            for i in program'range loop
+                retval(i) := program(i);
+            end loop;
+            return retval;
+        else
+            return program;
+        end if;
+        
+    end pipelined_block;
+------------------------------------------------------------------------
+    function pipelined_block
+    (
+        instruction : t_instruction
+    )
+    return program_array
+    is
+    begin
+        return pipelined_block(program_array'(0=>instruction));
+    end pipelined_block;
+------------------------------------------------------------------------
+    function "+"
+    (
+        left : variable_array; right : integer
+    )
+    return variable_array
+    is
+        variable retval : variable_array(left'range);
+    begin
+        for i in left'range loop
+            retval(i) := left(i) + right;
+        end loop;
+        return retval;
+    end "+";
+    function init_variables
+    (
+        number_of_variables : natural
+    )
+    return variable_array
+    is
+        variable retval : variable_array(0 to number_of_variables-1) := (others => 0);
+    begin
+        for i in retval'range loop
+            retval(i) := i;
+        end loop;
+
+        return retval;
+        
+    end init_variables;
+------------------------------------------------------------------------
+end package body microinstruction_pkg;
---
+++ vhdl2008/vhdl2008_microprogram_processor_pkg.vhd
@@ -0,0 +1,174 @@
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+    -- use work.microinstruction_pkg.all;
+    -- use work.multi_port_ram_pkg.all;
+    -- use work.processor_configuration_pkg.all;
+
+package microprogram_processor_pkg is
+
+    alias register_bit_width is work.processor_configuration_pkg.register_bit_width;
+
+    type microprogram_processor_record is record
+        processor_enabled    : boolean                    ;
+        is_ready             : boolean                    ;
+        program_counter      : natural range 0 to 1023    ;
+        registers            : reg_array                  ;
+        instruction_pipeline : instruction_pipeline_array ;
+    end record                                            ;
+
+    function init_processor return microprogram_processor_record;
+------------------------------------------------------------------------
+    procedure create_microprogram_processor (
+        signal self           : inout microprogram_processor_record ;
+        signal ram_read_out   : in ram_read_out_array         ;
+        signal ram_read_in    : out ram_read_in_array         ;
+        signal ram_write_port : out ram_write_in_record       ;
+        used_instruction      : inout t_instruction);
+
+------------------------------------------------------------------------     
+    procedure request_processor (
+        signal self : out microprogram_processor_record);
+------------------------------------------------------------------------
+    function program_is_ready ( self : microprogram_processor_record)
+        return boolean;
+------------------------------------------------------------------------
+    function processor_is_enabled (
+        self : microprogram_processor_record)
+    return boolean;
+------------------------------------------------------------------------
+    procedure request_processor (
+        signal self : out microprogram_processor_record;
+        program_start_address : in natural);
+
+end package microprogram_processor_pkg;
+
+package body microprogram_processor_pkg is
+
+------------------------------------------------------------------------     
+    function init_processor return microprogram_processor_record
+    is
+        constant zero_all : microprogram_processor_record :=
+        (
+            processor_enabled    => false                       , -- boolean                 ;
+            is_ready             => false                       , -- boolean                 ;
+            program_counter      => 0                           , -- natural range 0 to 1023 ;
+            registers            => (others => (others => '0')) , -- reg_array               ;
+            instruction_pipeline => (others => (others => '0')) ); -- instruction_pipeline_array       ;
+    begin
+
+        return zero_all;
+        
+    end init_processor;
+------------------------------------------------------------------------     
+    procedure create_microprogram_processor
+    (
+        signal self           : inout microprogram_processor_record ;
+        signal ram_read_out   : in ram_read_out_array         ;
+        signal ram_read_in    : out ram_read_in_array         ;
+        signal ram_write_port : out ram_write_in_record       ;
+        used_instruction      : inout t_instruction
+    ) is
+        alias ram_read_instruction_in is ram_read_in(0);
+        alias ram_read_instruction_out is ram_read_out(0);
+    begin
+        init_ram(ram_read_in, ram_write_port);
+    ------------------------------------------------------------------------
+        self.is_ready <= false;
+        used_instruction := write_instruction(nop);
+        if self.processor_enabled then
+            request_data_from_ram(ram_read_instruction_in, self.program_counter);
+
+            if ram_read_is_ready(ram_read_instruction_out) then
+                used_instruction := get_ram_data(ram_read_instruction_out);
+            end if;
+
+            if decode(used_instruction) = program_end then
+                self.processor_enabled <= false;
+                self.is_ready <= true;
+            else
+                self.program_counter <= self.program_counter + 1;
+            end if;
+        end if;
+        self.instruction_pipeline <= used_instruction & self.instruction_pipeline(0 to self.instruction_pipeline'high-1);
+    ------------------------------------------------------------------------
+    end create_microprogram_processor;
+
+    -- obsolete
+    procedure create_microprogram_processor
+    (
+        signal self                    : inout microprogram_processor_record ;
+        signal ram_read_instruction_in : out ram_read_in_record        ;
+        ram_read_instruction_out       : in ram_read_out_record        ;
+        signal ram_read_data_in        : out ram_read_in_record        ;
+        ram_read_data_out              : in ram_read_out_record        ;
+        signal ram_write_port          : out ram_write_in_record       ;
+        used_instruction               : inout t_instruction
+    ) is
+    begin
+        init_ram(ram_read_instruction_in, ram_read_data_in, ram_write_port);
+    ------------------------------------------------------------------------
+        self.is_ready <= false;
+        used_instruction := write_instruction(nop);
+        if self.processor_enabled then
+            request_data_from_ram(ram_read_instruction_in, self.program_counter);
+
+            if ram_read_is_ready(ram_read_instruction_out) then
+                used_instruction := get_ram_data(ram_read_instruction_out);
+            end if;
+
+            if decode(used_instruction) = program_end then
+                self.processor_enabled <= false;
+                self.is_ready <= true;
+            else
+                self.program_counter <= self.program_counter + 1;
+            end if;
+        end if;
+        self.instruction_pipeline <= used_instruction & self.instruction_pipeline(0 to self.instruction_pipeline'high-1);
+    ------------------------------------------------------------------------
+    end create_microprogram_processor;
+
+    function program_is_ready
+    (
+        self : microprogram_processor_record
+    )
+    return boolean
+    is
+    begin
+        return self.is_ready;
+        
+    end program_is_ready;
+
+    procedure request_processor
+    (
+        signal self : out microprogram_processor_record
+    ) is
+    begin
+        self.program_counter <= 0;
+        self.processor_enabled <= true;
+    end request_processor;
+
+    procedure request_processor
+    (
+        signal self : out microprogram_processor_record;
+        program_start_address : in natural
+    ) is
+    begin
+        self.program_counter <= program_start_address;
+        self.processor_enabled <= true;
+    end request_processor;
+
+------------------------------------------------------------------------     
+    function processor_is_enabled
+    (
+        self : microprogram_processor_record
+    )
+    return boolean
+    is
+    begin
+        return self.processor_enabled;
+    end processor_is_enabled;
+------------------------------------------------------------------------     
+end package body microprogram_processor_pkg;
