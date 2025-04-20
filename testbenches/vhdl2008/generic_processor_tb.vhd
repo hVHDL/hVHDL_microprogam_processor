3cc7933d60fd2399015fc5a6cb0f36ba1eda3c30
Jari Honkanen <jaarhonka@yahoo.com>
2025-04-20 22:21:39 +0300

    start moving add sub sum into own entity

--- testbenches/vhdl2008/generic_processor_tb.vhd
+++ testbenches/vhdl2008/generic_processor_tb.vhd
@@ -1,24 +1,102 @@
 
--- LIBRARY ieee  ; 
---     USE ieee.NUMERIC_STD.all  ; 
---     USE ieee.std_logic_1164.all  ; 
---     use ieee.math_real.all;
---
--- entity add_sub_mpy is
---     generic(
---         package microinstruction_pkg is new work.generic_microinstruction_pkg 
---         ;package mp_ram_pkg is new work.generic_multi_port_ram_pkg 
---        );
---     port(
---         clock : in std_logic
---     );
--- end entity add_sub_mpy;
---
--- architecture rtl of add_sub_mpy is
---
--- begin
---
--- end rtl;
+LIBRARY ieee  ; 
+    USE ieee.NUMERIC_STD.all  ; 
+    USE ieee.std_logic_1164.all  ; 
+    use ieee.math_real.all;
+
+entity instruction is
+    generic(
+        package microinstruction_pkg is new work.generic_microinstruction_pkg generic map (<>)
+        ;package mp_ram_pkg is new work.generic_multi_port_ram_pkg generic map (<>)
+       );
+    port(
+        clock : in std_logic
+        ;ram_read_in  : out mp_ram_pkg.ram_read_in_array
+        ;ram_read_out : in mp_ram_pkg.ram_read_out_array
+        ;ram_write_in : out mp_ram_pkg.ram_write_in_record
+        ;instr_pipeline : in microinstruction_pkg.instruction_pipeline_array
+    );
+    use microinstruction_pkg.all;
+    use mp_ram_pkg.all;
+    use work.real_to_fixed_pkg.all;
+end;
+
+architecture add_sub_mpy of instruction is
+
+    signal a, b, c: signed(31 downto 0);
+    signal mpy_res : signed(63 downto 0);
+
+begin
+
+    mpy_add_sub : process(clock) is
+    begin
+        if rising_edge(clock) then
+            init_mp_ram_read(ram_read_in);
+            init_mp_write(ram_write_in);
+
+            ---------------
+            if ram_read_is_ready(ram_read_out(4)) then
+                CASE decode(get_ram_data(ram_read_out(4))) is
+                    WHEN add =>
+                        request_data_from_ram(ram_read_in(2)
+                        , get_arg1(get_ram_data(ram_read_out(4))));
+
+                        request_data_from_ram(ram_read_in(1)
+                        , get_arg2(get_ram_data(ram_read_out(4))));
+
+                    WHEN sub =>
+                        request_data_from_ram(ram_read_in(2)
+                        , get_arg1(get_ram_data(ram_read_out(4))));
+
+                        request_data_from_ram(ram_read_in(1)
+                        , get_arg2(get_ram_data(ram_read_out(4))));
+
+                    WHEN mpy_add =>
+                        request_data_from_ram(ram_read_in(0)
+                        , get_arg1(get_ram_data(ram_read_out(4))));
+
+                        request_data_from_ram(ram_read_in(1)
+                        , get_arg2(get_ram_data(ram_read_out(4))));
+
+                        request_data_from_ram(ram_read_in(2)
+                        , get_arg3(get_ram_data(ram_read_out(4))));
+
+                    WHEN others => -- do nothing
+                end CASE;
+            end if;
+            ---------------
+            mpy_res <= a * b + resize(shift_left(c , 14) , 63);
+
+            CASE decode(instr_pipeline(2)) is
+                WHEN add =>
+                    a <= to_fixed(1.0, 32, 14);
+                    b <= signed(get_ram_data(ram_read_out(1)));
+                    c <= signed(get_ram_data(ram_read_out(2)));
+
+                WHEN sub =>
+                    a <= to_fixed(1.0, 32, 14);
+                    b <=  signed(get_ram_data(ram_read_out(1)));
+                    c <= -signed(get_ram_data(ram_read_out(2)));
+
+                WHEN mpy_add =>
+                    a <= signed(get_ram_data(ram_read_out(0)));
+                    b <= signed(get_ram_data(ram_read_out(1)));
+                    c <= signed(get_ram_data(ram_read_out(2)));
+
+                WHEN others => -- do nothing
+            end CASE;
+            ---------------
+            CASE decode(instr_pipeline(4)) is
+                WHEN add | sub | mpy_add =>
+                    write_data_to_ram(ram_write_in , get_dest(instr_pipeline(4)), std_logic_vector(mpy_res(14+31 downto 14)));
+                WHEN others => -- do nothing
+            end CASE;
+            ---------------
+
+        end if;
+    end process mpy_add_sub;
+
+end add_sub_mpy;
 
 --------------------------------------------------
 LIBRARY ieee  ; 
@@ -155,73 +233,9 @@ begin
     end process make_pipeline;
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
-    mpy_add_sub : process(simulator_clock) is
-    begin
-        if rising_edge(simulator_clock) then
-            init_mp_ram_read(sub_read_in);
-            init_mp_write(add_sub_ram_write);
-
-            ---------------
-            if ram_read_is_ready(ram_read_out(4)) then
-                CASE decode(get_ram_data(ram_read_out(4))) is
-                    WHEN add =>
-                        request_data_from_ram(sub_read_in(2)
-                        , get_arg1(get_ram_data(ram_read_out(4))));
-
-                        request_data_from_ram(sub_read_in(1)
-                        , get_arg2(get_ram_data(ram_read_out(4))));
-
-                    WHEN sub =>
-                        request_data_from_ram(sub_read_in(2)
-                        , get_arg1(get_ram_data(ram_read_out(4))));
-
-                        request_data_from_ram(sub_read_in(1)
-                        , get_arg2(get_ram_data(ram_read_out(4))));
-
-                    WHEN mpy_add =>
-                        request_data_from_ram(sub_read_in(0)
-                        , get_arg1(get_ram_data(ram_read_out(4))));
-
-                        request_data_from_ram(sub_read_in(1)
-                        , get_arg2(get_ram_data(ram_read_out(4))));
-
-                        request_data_from_ram(sub_read_in(2)
-                        , get_arg3(get_ram_data(ram_read_out(4))));
-
-                    WHEN others => -- do nothing
-                end CASE;
-            end if;
-            ---------------
-            mpy_res <= a * b + resize(shift_left(c , 14) , 63);
-
-            CASE decode(instr_pipeline(2)) is
-                WHEN add =>
-                    a <= to_fixed(1.0, 32, 14);
-                    b <= signed(get_ram_data(ram_read_out(1)));
-                    c <= signed(get_ram_data(ram_read_out(2)));
-
-                WHEN sub =>
-                    a <= to_fixed(1.0, 32, 14);
-                    b <=  signed(get_ram_data(ram_read_out(1)));
-                    c <= -signed(get_ram_data(ram_read_out(2)));
-
-                WHEN mpy_add =>
-                    a <= signed(get_ram_data(ram_read_out(0)));
-                    b <= signed(get_ram_data(ram_read_out(1)));
-                    c <= signed(get_ram_data(ram_read_out(2)));
-
-                WHEN others => -- do nothing
-            end CASE;
-            ---------------
-            CASE decode(instr_pipeline(4)) is
-                WHEN add | sub | mpy_add =>
-                    write_data_to_ram(add_sub_ram_write , get_dest(instr_pipeline(4)), std_logic_vector(mpy_res(14+31 downto 14)));
-                WHEN others => -- do nothing
-            end CASE;
-            ---------------
-
-        end if;
-    end process mpy_add_sub;
+    add_sub_mpy : entity work.instruction
+    generic map(microinstruction_pkg, mp_ram_pkg)
+    port map(simulator_clock , sub_read_in , ram_read_out , add_sub_ram_write , instr_pipeline);
 ------------------------------------------------------------------------
     ram_read_in <= pim_read_in and sub_read_in;
     ram_write_in <= pim_ram_write and add_sub_ram_write ;
