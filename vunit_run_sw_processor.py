fccea0464d8319ae42f2e84db8338cb53979fe71
Jari Honkanen <jaarhonka@yahoo.com>
2025-04-29 18:02:47 +0300

    move vhdl2008 microprogram processor sources out of testbench folder

--- vunit_run_sw_processor.py
+++ vunit_run_sw_processor.py
@@ -93,15 +93,15 @@ v2008.add_source_files(ROOT / "source/hVHDL_memory_library/multi_port_ram/generi
 
 v2008.add_source_files(ROOT / "vhdl2008/vhdl2008_microinstruction_pkg.vhd")
 v2008.add_source_files(ROOT / "vhdl2008/vhdl2008_microprogram_processor_pkg.vhd")
+v2008.add_source_files(ROOT / "vhdl2008/microprogram_sequencer.vhd")
+v2008.add_source_files(ROOT / "vhdl2008/add_sub_instruction.vhd")
+v2008.add_source_files(ROOT / "vhdl2008/microprogram_processor.vhd")
+v2008.add_source_files(ROOT / "vhdl2008/ram_connector_generic_pkg.vhd")
 
 v2008.add_source_files(ROOT / "testbenches/vhdl2008/generic_processor_tb.vhd")
 v2008.add_source_files(ROOT / "testbenches/vhdl2008/microprogram_processor_tb.vhd")
 v2008.add_source_files(ROOT / "testbenches/vhdl2008/mproc_test_modeling_tb.vhd")
 # refactor these
-v2008.add_source_files(ROOT / "testbenches/vhdl2008/microprogram_sequencer.vhd")
-v2008.add_source_files(ROOT / "testbenches/vhdl2008/add_sub_instruction.vhd")
-v2008.add_source_files(ROOT / "testbenches/vhdl2008/microprogram_processor.vhd")
-v2008.add_source_files(ROOT / "testbenches/vhdl2008/ram_connector_generic_pkg.vhd")
 
 # VU.set_sim_option("nvc.sim_flags", ["-w", "--dump-arrays"])
 VU.set_sim_option("nvc.sim_flags", ["-w"])
